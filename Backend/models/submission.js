36e12a46aea54e5778287ac03f97460b7566a354
Rahul <rahulramtekehu@gmail.com>
2026-02-18 09:57:14 +0530

    Last faculty commit

--- Backend/models/submission.js
+++ Backend/models/submission.js
@@ -8,30 +8,49 @@ const submissionSchema = new mongoose.Schema(
       required: true,
       index: true,
     },
+
     student: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Student",
+      ref: "User",
       required: true,
       index: true,
     },
+
     fileUrl: {
       type: String,
       required: true,
-      trim: true,
     },
+
+    fileType: {
+      type: String,
+      enum: ["pdf", "img", "docs", "ppt"],
+      required: true,
+    },
+
     marks: {
       type: Number,
       default: null,
     },
+
     feedback: {
       type: String,
-      default: "",
       trim: true,
     },
+
+    status: {
+      type: String,
+      enum: ["submitted", "late"],
+      default: "submitted",
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-submissionSchema.index({ assignment: 1, student: 1 }, { unique: true });
+submissionSchema.index(
+  { assignment: 1, student: 1 },
+  { unique: true }
+);
 
 export default mongoose.model("Submission", submissionSchema);
