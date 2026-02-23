53da0badd8757b3cf4cec95f680cd12469fc2ab2
Subesh Sarkar <work.subesh@gmail.com>
2026-02-23 14:48:26 +0530

    Added new changes

--- hu-erp1 (2)/hu-erp1/Frontend/src/components/faculty/Toast.jsx
+++
@@ -1,9 +0,0 @@
-export default function Toast({ toast }) {
-  return (
-    <div
-      className={`faculty-toast ${toast.show ? "show" : "hide"} ${toast.type}`}
-    >
-      {toast.text}
-    </div>
-  );
-}
